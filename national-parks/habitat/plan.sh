c07b175f11d51bdf6d9005e6200ce674b57f622d
Habitat User <hab@ip-10-0-1-172.us-west-2.compute.internal>
2018-05-22 20:10:18 +0000

    national parks working

---
+++ national-parks/habitat/plan.sh
@@ -0,0 +1,28 @@
+pkg_name=national-parks
+pkg_origin=taksan
+pkg_version="7.3.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
+pkg_build_deps=(core/jdk8/8u131 core/maven)
+pkg_svc_user="root"
+do_prepare(){ export JAVA_HOME=$(hab pkg path core/jdk8) ; }
+
+pkg_binds=(
+    [database]="port"
+)
+
+do_build()
+{
+    cp -r $PLAN_CONTEXT/../ $HAB_CACHE_SRC_PATH/$pkg_dirname
+    cd ${HAB_CACHE_SRC_PATH}/${pkg_dirname}
+    mvn package
+}
+
+do_install()
+{
+    mkdir ${PREFIX}/config
+    cp ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/target/${pkg_name}.war ${PREFIX}/
+    cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
+    cp -v ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/data/national-parks.json ${PREFIX}/
+}
