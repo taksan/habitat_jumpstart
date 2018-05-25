b3c06777205821b4dd405c1003bcbaf3553ab49b
takeuchi <g.takeuchi@gmail.com>
2018-05-25 14:44:00 -0500

    jdk fix

--- national-parks/habitat/plan.sh
+++ national-parks/habitat/plan.sh
@@ -4,7 +4,7 @@ pkg_version="7.3.0"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_deps=(core/tomcat8 core/jre8 core/mongo-tools)
-pkg_build_deps=(core/jdk8/8u131 core/maven)
+pkg_build_deps=(core/jdk8/8.172.0 core/maven)
 pkg_svc_user="root"
 do_prepare(){ export JAVA_HOME=$(hab pkg path core/jdk8) ; }
 
@@ -26,3 +26,4 @@ do_install()
     cp $(hab pkg path core/tomcat8)/config/conf_server.xml ${PREFIX}/config/
     cp -v ${HAB_CACHE_SRC_PATH}/${pkg_dirname}/data/national-parks.json ${PREFIX}/
 }
+#just testing
